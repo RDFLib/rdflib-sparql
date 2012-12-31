48185bd1f44149e7681a2a3735bdf34d9c612946
Graham Higgins <gjh-github@bel-epa.com>
2012-12-31 04:29:05 +0000

    Add Py3.3 to tox.ini and build/src flush to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-import sys, re
+import sys
+import re
 from setuptools import setup, find_packages
 
 # Find version. We have to do this because we can't import it in Python 3 until
 # its been automatically converted in the setup process.
+
+
 def find_version(filename):
     _version_re = re.compile(r'__version__ = "(.*)"')
     for line in open(filename):
@@ -12,13 +15,16 @@ def find_version(filename):
 
 version = find_version('rdflib_sparql/__init__.py')
 
+
 def setup_python3():
     # Taken from "distribute" setup.py
     from distutils.filelist import FileList
     from distutils import dir_util, file_util, util, log
-    from os.path import join
-  
+    from os.path import join, exists
+
     tmp_src = join("build", "src")
+    if exists(tmp_src):
+        dir_util.remove_tree(tmp_src)
     log.set_verbosity(1)
     fl = FileList()
     for line in open("MANIFEST.in"):
@@ -31,39 +37,39 @@ def setup_python3():
         outf, copied = file_util.copy_file(f, join(tmp_src, f), update=1)
         if copied and outf.endswith(".py"):
             outfiles_2to3.append(outf)
-  
+
     util.run_2to3(outfiles_2to3)
-  
+
     # arrange setup to use the copy
     sys.path.insert(0, tmp_src)
-  
+
     return tmp_src
 
 
-requires = [ 'rdflib>3.2' ]
+requires = ['rdflib>3.2']
 
-kwargs={}
+kwargs = {}
 
-if sys.version_info[:2]<(2,7): 
-    requires.append("ordereddict")    
+if sys.version_info[:2] < (2, 7):
+    requires.append("ordereddict")
 
 if sys.version_info[0] >= 3:
     kwargs['use_2to3'] = True
     kwargs['src_root'] = setup_python3()
     requires.append("pyparsing")
-else: 
+else:
     requires.append("pyparsing<=1.5.7")
 
 setup(
-    name = "rdflib-sparql",
-    author = "Gunnar Aastrand Grimnes", 
-    author_email = "gromgull@gmail.com",
+    name="rdflib-sparql",
+    author="Gunnar Aastrand Grimnes",
+    author_email="gromgull@gmail.com",
     url="http://github.com/RDFLib/rdflib-sparql",
-    version = version,
-    packages = find_packages(),
-    install_requires = requires,
-    tests_require = [ 'nose' ],
-    entry_points = {
+    version=version,
+    packages=find_packages(),
+    install_requires=requires,
+    tests_require=['nose'],
+    entry_points={
         'rdf.plugins.queryresult': [
             'sparql = rdflib_sparql.processor:SPARQLResult',
         ],
@@ -85,6 +91,6 @@ setup(
             'tsv = rdflib_sparql.results.tsvresults:TSVResultParser',
 
         ],
-        },
+    },
     **kwargs
 )
