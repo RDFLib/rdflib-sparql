d29b87e580e768cda0151b931d9617f27010f539
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2013-03-03 10:24:36 +0100

    ... and added missing file

---
+++ rdflib_sparql/py3compat.py
@@ -0,0 +1,12 @@
+"""
+utilities for easing py3 transition for rdflib_sparql
+"""
+
+from rdflib import py3compat
+
+def decodeStringEscape(s): 
+    if not py3compat.PY3:
+        return s.decode('string-escape')
+    else:
+        # I love py3, isn't this marvellously convenient? 
+        return bytes(s, "utf-8").decode("unicode_escape")
