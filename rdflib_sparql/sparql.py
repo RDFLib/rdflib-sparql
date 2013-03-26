8262e24f0eb3d1b5674547f45b663c272be0511e
Joern Hees <gitdev@joernhees.de>
2013-03-26 02:43:00 +0100

    fix for python 2.5 compatibility problem
    FrozenDict is based on Mapping which was only added to collections in
    2.6. compat.py includes some compatibility code to base it on
    MutableMapping, basing on DictMixin from UserDict, which is an old-style
    class causing super not to work properly :-/

--- rdflib_sparql/sparql.py
+++ rdflib_sparql/sparql.py
@@ -148,7 +148,7 @@ class FrozenDict(Mapping):
 class FrozenBindings(FrozenDict):
 
     def __init__(self, ctx, *args, **kwargs):
-        super(FrozenBindings, self).__init__(*args, **kwargs)
+        FrozenBindings.__init__(self, *args, **kwargs)
         self.ctx = ctx
 
     def __getitem__(self, key):
